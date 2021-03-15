26e6b31a6c18871c20341e6ca2c811cf501d8927
muratniyazov <gaday1037@gmail.com>
2021-03-15 21:35:35 +0300

    First commit murat

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup from murat
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in development MURAT
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
